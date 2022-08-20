d9d6579ef42136bba4f5547631dfd1cbe85cd4d0
strongwillow <clear.tsai@gmail.com>
2022-08-20 14:48:23 +0200

    update version

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 npm run build
+
+ls
+
 cd build
+
+ls -la
+
 mv script.js tmp.js
 sed "s/710cc921fda7d757cc9b0aecd40ad3be/d750a1a29e913dea376aca86cc95de5a/" tmp.js > tmp2
 sed "s/lz4.overpass-api.de/overpass.tracestrack.com/" tmp2 > script.js
