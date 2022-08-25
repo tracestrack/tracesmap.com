3d40c53d0287ed3548bc3e60e233bd5327c0cffd
strongwillow <clear.tsai@gmail.com>
2022-08-25 22:35:59 +0200

    update build

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ npm run build
 
 ls
 
-#cd build
+cd build
 
-#mv script.js tmp.js
-#sed "s/710cc921fda7d757cc9b0aecd40ad3be/d750a1a29e913dea376aca86cc95de5a/" tmp.js > tmp2
+mv script.js tmp.js
+sed "s/710cc921fda7d757cc9b0aecd40ad3be/d750a1a29e913dea376aca86cc95de5a/" tmp.js > script.js
 #sed "s/lz4.overpass-api.de/overpass.tracestrack.com/" tmp2 > script.js
 
-#rm tmp2
+rm tmp.js
