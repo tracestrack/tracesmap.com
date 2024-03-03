fd7cf8d481e1d0350fcdf31898cbaabf9b332fe5
Quinn <clear.tsai@gmail.com>
2024-03-03 21:42:40 +0100

    simplify build

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
 npm run build
-
-ls
-
-cd build
-
-mv script.js tmp.js
-sed "s/710cc921fda7d757cc9b0aecd40ad3be/d750a1a29e913dea376aca86cc95de5a/" tmp.js > script.js
-#sed "s/lz4.overpass-api.de/overpass.tracestrack.com/" tmp2 > script.js
-
-rm tmp.js
